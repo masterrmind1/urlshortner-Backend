cbe6c1abe4a1c13d7125dc9a81fe7c2bfe03304f
Masterrmind1 <rajputp2007@gmail.com>
2023-01-06 18:18:33 +0530

    updated1

--- src/router/user_verification/reset-password.js
+++ src/router/user_verification/reset-password.js
@@ -1,11 +1,10 @@
-const { ObjectId } = require('mongodb')
 const express = require('express')
 const router = new express.Router()
 const auth = require('../../middleware/auth')
 const resetPasswordController = require('../../controller/userVerification/reserPassword.controller')
 
 //input - email, currentPassword, newPassword
-router.patch('/reset-password/:id', auth, resetPasswordController.resetPasswordControllerWithEmail)
+router.patch('/reset-password/:id', resetPasswordController.resetPasswordControllerWithEmail)
 
 router.patch('/reset/:urlId', resetPasswordController.resetPasswordControllerWithUrlId)
 
